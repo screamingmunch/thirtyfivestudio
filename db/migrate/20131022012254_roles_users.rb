d14e4b2527bcbfd66048d71fd6cce28f347da820
Kristine <screamingmunch@gmail.com>
2013-10-23 12:45:00 -0700

    added twitter omniauth (not working) includes deleted files

--- db/migrate/20131022012254_roles_users.rb
+++
@@ -1,7 +0,0 @@
-class RolesUsers < ActiveRecord::Migration
-  def up
-  end
-
-  def down
-  end
-end
--- db/migrate/20131022031639_add_description_to_gears.rb
+++
@@ -1,5 +0,0 @@
-class AddDescriptionToGears < ActiveRecord::Migration
-  def change
-    add_column :gears, :Description, :text
-  end
-end
