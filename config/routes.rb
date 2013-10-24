9009be7fb3339b3439ffe868a084002c48b465fb
Kristine <screamingmunch@gmail.com>
2013-10-24 13:20:33 -0700

    inquiry form done but signs user out upon submit

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,16 @@ RedDevise1::Application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
                    controllers: {omniauth_callbacks: "omniauth_callbacks"}
 
+  post '/events/inquiry' => 'events#inquiry'
+
+  get '/events/thankyou' => 'events#thankyou'
+  get '/about' => 'home#about'
+  
   resources :gears, :users, :events, :galleries
 
-  get '/about' => 'home#about'
+  
+
+
 
   match 'auth/:provider/callback', to: 'sessions#create'
 	# match 'auth/failure', to: redirect('/')
